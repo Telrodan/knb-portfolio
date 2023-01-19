bb22608a402cf19b8ae38fd8c6edd030a97a1224
bence keresztes-nagy <telrodan@gmail.com>
2023-01-19 15:41:50 +0100

    todo list finished

--- src/app/features/works/WORKS_DATA.ts
+++ src/app/features/works/WORKS_DATA.ts
@@ -4,7 +4,8 @@ export const works = [
     projectDescription:
       'In this project, I have created a to-do list manager using RxJS and services. The service handles data logic and RxJS observables for real-time updates. User interface is built using RxJS for displaying and interacting with the lists. It includes features such as adding, editing, deleting and marking items. Overall, it provides an efficient and user-friendly to-do list application.',
     projectImage: 'todo-list-image.png',
-    projectCode: '',
+    projectCode:
+      'https://github.com/Telrodan/knb-portfolio/tree/main/src/app/features/works/todo-list',
     projectDemo: 'demo/todo-list'
   }
 ];
